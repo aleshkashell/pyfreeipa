e7030eb1d83815cb2339437dcf9c04f73fc157e9
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2018-10-05 16:56:33 +0300

    info for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyfreeipa',
+  packages = ['pyfreeipa'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Module for work with freeipa',
+  author = 'Sheludchenkov Aleksey',
+  author_email = 'aleshkashell@gmail.com',
+  url = 'https://github.com/aleshkashell/pyfreeipa',
+  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1',
+  keywords = ['add', 'sub', 'tests', 'freeipa'], 
+  classifiers = [],
+)
