6be8673a83af49dd2272a6b78e6f824134e99772
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-09 17:12:41 +0300

    delete extra dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sheludchenkov Aleksey',
   author_email = 'aleshkashell@gmail.com',
   url = 'https://github.com/aleshkashell/pyfreeipa',
-  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1.3',
+  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1.4',
   keywords = ['add', 'sub', 'tests', 'freeipa'], 
   classifiers = [],
   install_requires=[
@@ -15,7 +15,4 @@ setup(
           'kerberos',
           'dnspython',
       ],
-  dependency_links = [
-    'https://github.com/nordnet/python-freeipa-json.git#egg=python-freeipa-json'
-  ]
 )
