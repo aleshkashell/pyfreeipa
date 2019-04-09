c4b7638379829bb13d5f118208d9aa6e8c0d569c
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-09 17:16:38 +0300

    delete extra dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyfreeipa',
   packages = ['pyfreeipa'], # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'Module for work with freeipa',
   author = 'Sheludchenkov Aleksey',
   author_email = 'aleshkashell@gmail.com',
   url = 'https://github.com/aleshkashell/pyfreeipa',
-  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1.3',
+  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1.4',
   keywords = ['add', 'sub', 'tests', 'freeipa'], 
   classifiers = [],
   install_requires=[
@@ -15,7 +15,4 @@ setup(
           'kerberos',
           'dnspython',
       ],
-  dependency_links = [
-    'https://github.com/nordnet/python-freeipa-json.git#egg=python-freeipa-json'
-  ]
 )
