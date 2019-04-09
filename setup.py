d17d6873fbb0da301806c0661c73d91856e953b9
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-09 15:51:11 +0300

    add requirements 0.1.3 and update version

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from distutils.core import setup
 setup(
   name = 'pyfreeipa',
   packages = ['pyfreeipa'], # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Module for work with freeipa',
   author = 'Sheludchenkov Aleksey',
   author_email = 'aleshkashell@gmail.com',
   url = 'https://github.com/aleshkashell/pyfreeipa',
-  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1',
+  download_url = 'https://github.com/aleshkashell/pyfreeipa/tarball/0.1.3',
   keywords = ['add', 'sub', 'tests', 'freeipa'], 
   classifiers = [],
+  install_requires=[
+          'requests',
+          'kerberos',
+          'dnspython',
+      ],
+  dependency_links = [
+    'https://github.com/nordnet/python-freeipa-json.git#egg=python-freeipa-json'
+  ]
 )
